142e0d767674f68a4b0ccc4cb987dfd44e7aea0b
nickrom <romanov_niki@mail.ru>
2015-11-06 09:15:35 +0300

    Update TOneDimensionNormalDistribution.cpp

--- MathModules/TOneDimensionNormalDistribution.cpp
+++ MathModules/TOneDimensionNormalDistribution.cpp
@@ -44,26 +44,99 @@ TOneDimensionNormalDistribution& TOneDimensionNormalDistribution::SetParameter_M
         return *this;
 }
 
-//add new Exception
-OneDimensionNormalDistribution& TOneDimensionNormalDistribution::SetParameter_Sigma(long double value_sigma) {
-	if (value_sigma <= 0.0)
-		throw TInvalidParametersException;
+TOneDimensionNormalDistribution& TOneDimensionNormalDistribution::SetParameter_Sigma(long double value_sigma) {
+        if (value_sigma <= 0.0)
+                throw TInvalidParametersException;
 
-	_sigma = value_sigma;
-	return *this;
+        _sigma = value_sigma;
+        return *this;
 }
 
 long double TOneDimensionNormalDistribution::_Density(long double x) {
         //return boost::math::pdf(x,_m,_sigma);
-        return 1/(_sigma * sqrt(2*M_PI)) * exp((-(x-_m)*(x-_m)) / (2 * _sigma * _sigma));
+        return 1/(sqrt(2*M_PI*_sigma)) * exp((-(x-_m)*(x-_m)) / (2 * _sigma));
 }
 
 long double TOneDimensionNormalDistribution::_Function(long double _x) {
         //return boost::math::cdf()
-        return (1+erfl((_x-_m)/sqrtl(2*_sigma*_sigma)))/2.0;
+        return (1+erfl((_x-_m)/sqrtl(2*_sigma)))/2.0;
 }
 
 long double TOneDimensionNormalDistribution::_IntervalProbability(long double _left, long double _right) {
-        //boost::math::
-        return 0.0;
+        return _Function(_right)-_Function(_left);
+}
+
+long double TOneDimensionNormalDistribution::_ExpectedValue() {
+        return _m;
+}
+
+long double TOneDimensionNormalDistribution::_Dispersion() {
+        return _sigma;
+}
+
+long double TOneDimensionNormalDistribution::_StandardDeviation() {
+        //???
+}
+
+long double TOneDimensionNormalDistribution::GetDensity(double x) {
+        return _Density((long double)x);
+}
+
+long double TOneDimensionNormalDistribution::GetDensity(long double x) {
+        return _Density(x);
+}
+
+long double TOneDimensionNormalDistribution::GetFunctionValue(double x) {
+        return _Function((long double)x);
+}
+
+long double TOneDimensionNormalDistribution::GetFunctionValue(long double x) {
+        return _Function(x);
+}
+
+long double TOneDimensionNormalDistribution::GetFunctionValue(unsigned n) {
+        return _Function((long double)n);
+}
+
+long double TOneDimensionNormalDistribution::GetFunctionValue(unsigned long long n) {
+        return _Function((long double)n);
+}
+
+long double TOneDimensionNormalDistribution::GetCumulativeFunctionValue(double x) {
+        //???
+}
+
+long double TOneDimensionNormalDistribution::GetCumulativeFunctionValue(long double x) {
+        //???
+}
+
+long double TOneDimensionNormalDistribution::GetCumulativeFunctionValue(unsigned n) {
+        //???
+}
+
+long double TOneDimensionNormalDistribution::GetCumulativeFunctionValue(unsigned long long n) {
+        //???
+}
+
+long double TOneDimensionNormalDistribution::GetIntervalProbability(double LeftBound, double RightBound) {
+        return GetIntervalProbability((long double) LeftBound, (long double) RightBound);
+}
+
+long double TOneDimensionNormalDistribution::GetIntervalProbability(long double LeftBound, long double RightBound) {
+        if (LeftBound<RightBound)
+                return _IntervalProbability(LeftBound, RightBound);
+        else
+                return _IntervalProbability(RightBound, LeftBound);
+}
+
+long double TOneDimensionNormalDistribution::GetExpectedValue() {
+        return _ExpectedValue();
+}
+
+long double TOneDimensionNormalDistribution::GetDispersion() {
+        return _Dispersion();
+}
+
+long double TOneDimensionNormalDistribution::GetStandardDeviation() {
+        return _StandardDeviation();
 }
